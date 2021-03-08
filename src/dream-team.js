66a5592110038b3eda2135ca34af1ac6499b7843
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2021-03-08 03:46:08 +0300

    feat: implement function dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let dreamTeamName = members
+    .filter(el => typeof el === 'string')
+    .map(el => el
+      .trim()
+      .toUpperCase()
+      .slice(0, 1))
+    .sort()
+    .join('');
+
+  return dreamTeamName;
 };
