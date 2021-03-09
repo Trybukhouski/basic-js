32e4a5d6e22b3f09e639674744738b4c364d42b1
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2021-03-10 01:25:07 +0300

    feat: implement task vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -4,4 +4,23 @@ module.exports = function repeater(/* str, options */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
-  
+
+// const CustomError = require("../extensions/custom-error");
+
+// module.exports = function repeater(str, options) {
+
+//   let finalAddition = `--separator--${options.addition || ''}`
+//     .repeat(+options.additionRepeatTimes || 1)
+//     .split('--separator--')
+//     .slice(1)
+//     .join(String(options.additionSeparator || '|'))
+
+
+//   let finalString = `--separator--${str}${finalAddition}`
+//     .repeat(+options.repeatTimes || 1)
+//     .split('--separator--')
+//     .slice(1)
+//     .join(String(options.separator || '+'));
+
+//   return finalString
+// };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(toReverse = true) {
+    this.toReverse = toReverse;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error();
+
+    const value = [];
+    let length = 0;
+    message = message.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+
+    for (let i = 0; i < message.length; i++) {
+
+      length > key.length - 1 ? length = 0 : length;
+
+      /[A-Z]/.test(message[i]) ?
+        value.push((message[i].charCodeAt() + key[length].charCodeAt()) % 26 + 65) && length++ :
+        value.push(message[i].charCodeAt());
+    }
+
+    return this.toReverse ?
+      String.fromCharCode(...value) :
+      String.fromCharCode(...value.reverse());
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error();
+
+    const value = [];
+    let length = 0;
+    encryptedMessage = encryptedMessage.toUpperCase().split('');
+    key = key.toUpperCase().split('');
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+
+      length > key.length - 1 ? length = 0 : length;
+
+      /[A-Z]/.test(encryptedMessage[i]) ?
+        value.push((encryptedMessage[i].charCodeAt() - key[length].charCodeAt() + 26) % 26 + 65) && length++ :
+        value.push(encryptedMessage[i].charCodeAt());
+    }
+
+    return this.toReverse ?
+      String.fromCharCode(...value) :
+      String.fromCharCode(...value.reverse());
   }
 }
 
