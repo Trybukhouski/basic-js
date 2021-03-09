cc60837c2f3790acbfcadf3e3dcf67c96af5e0cb
Ruslan Trybukhouski <trybukhouskij@gmail.com>
2021-03-10 02:38:59 +0300

    feat: implement task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,26 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let {
+    repeatTimes = 1,
+    additionRepeatTimes = 1,
+    addition = '',
+    separator = '+',
+    additionSeparator = '|'
+  } = options;
+
+  if (repeatTimes === undefined) repeatTimes = 1;
+  if (str === null) str = 'null';
+  if (addition === null) addition = 'null';
+  if (typeof addition === 'boolean') addition = String(addition);
+
+
+  let finalAddition = `--separator--${addition}`
+    .repeat(additionRepeatTimes)
+    .split('--separator--')
+    .slice(1)
+    .join(additionSeparator);
+
+
+  let finalString = `--separator--${str}${finalAddition}`
+    .repeat(repeatTimes)
+    .split('--separator--')
+    .slice(1)
+    .join(separator);
+
+  return finalString
 };
-
-// const CustomError = require("../extensions/custom-error");
-
-// module.exports = function repeater(str, options) {
-
-//   let finalAddition = `--separator--${options.addition || ''}`
-//     .repeat(+options.additionRepeatTimes || 1)
-//     .split('--separator--')
-//     .slice(1)
-//     .join(String(options.additionSeparator || '|'))
-
-
-//   let finalString = `--separator--${str}${finalAddition}`
-//     .repeat(+options.repeatTimes || 1)
-//     .split('--separator--')
-//     .slice(1)
-//     .join(String(options.separator || '+'));
-
-//   return finalString
-// };
